40df67362f9f1579c69561c891f79706bf90861e
fortin <fortin@ttu.ee>
2021-01-13 11:19:09 +0200

    Nice printing start but reading too many values

--- xl_reader.py
+++ xl_reader.py
@@ -17,19 +17,27 @@ def check_file(fname):
         return fname
 
 
-def xlsx_reader(header_row, fn):
+def xlsx_reader(header_row, data_row, data_col, fn):
     workbook = load_workbook(filename=fn, data_only=True)
     sheet = workbook.active
     header = list(list(sheet.iter_rows(min_row=header_row, max_row=header_row, min_col=2))[0])
-    print(header)
+
     result = []
-    for row in sheet.iter_rows(min_row=header_row + 1, min_col=2):
+    buff_str = ''
+    line_count = 0
+    for row in sheet.iter_rows(min_row=data_row, min_col=data_col):
+        line_count += 1
         buffer = {}
         for current_cell in row:
-            print('{}, {}'.format(current_cell.column, (current_cell.column-1)))
-            buffer[header[current_cell.column-2].value] = current_cell.value
-        print(buffer)
+            e = header[current_cell.column-2].value
+            buffer[e] = current_cell.value
+            buff_str += '{0:<25s}'.format(str(buffer[e])[:24])
+        buff_str += '\n'
         result.append(buffer)
+    
+    print('\n\n' + buff_str)
+    print('\nProcessed {} lines\n\n'.format(line_count))
+    
     return result
 
 
@@ -38,7 +46,7 @@ if __name__ == "__main__":
         current_month = '{:02d}'.format( int( input("Please enter month:")))
         xl_name = input("Please enter xl file name:")
         file_name = ud.cwd + ud.fd + ud.data_path + ud.fd + current_month + ud.fd + xl_name
-        result = xlsx_reader(6, check_file(file_name))
+        result = xlsx_reader(6, 8, 2, check_file(file_name))
         print('List of lenght: ' + str(len(result)))
     elif sys.argv[1] == "hello":
         print("hello world!")
