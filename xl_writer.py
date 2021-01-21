965bfae6d62012a625f445363168632eb48da067
fortin <fortin@ttu.ee>
2021-01-21 18:34:19 +0200

    xl_writer untested

--- xl_writer.py
+++ xl_writer.py
@@ -7,8 +7,21 @@ import user_data as ud
 from xl_helpers import check_file
 
 
-def xl_writer(current_month):
+def xl_writer(current_month, data,  fn=ud.fn):
     workbook = load_workbook(filename=fn, data_only=True)
     sheet = workbook[str(current_month)]
-    
+    for element in data:
+        f_append(sheet, element)
+    workbook.save(fn)
+
+
+def f_append(sheet, element):
+    row = []
+    for e in ud.xl_month_fileds:
+        try:
+            current = element[e]
+        except:
+            current = ""
+        row.append(current)
+    sheet.append(row)
     
